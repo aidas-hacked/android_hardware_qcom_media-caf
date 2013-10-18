ca8d4321f8e34bce38c32ab6545f0b00186f6b88
RonGokhale <cips173@gmail.com>
2013-10-18 22:33:06 +0530

    media: update for media-caf

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
 ifneq ($(filter msm8960 msm8660 msm7x30 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
